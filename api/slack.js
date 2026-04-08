179fc7c9fa2d7e34bd6e5e30b4189ae2ef544742
marketeryea-hub <marketer.yea@gmail.com>
2026-04-08 13:06:19 +0900

    Update slack.js

--- api/slack.js
+++ api/slack.js
@@ -11,7 +11,7 @@ module.exports = function handler(req, res) {
     return res.status(405).json({ error: 'POST만 가능합니다' });
   }
 
-  const WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+  const WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
   fetch(WEBHOOK_URL, {
     method: 'POST',
